f8c9966adf4788d8fe0503e1772f752c5d970221
adonixis_cvs <ilya.fomenko@compvisionsys.com>
2024-10-28 20:10:16 +0500

    Add gray color

--- app/src/main/java/com/bank/yandexlivepictures/ui/theme/Color.kt
+++ app/src/main/java/com/bank/yandexlivepictures/ui/theme/Color.kt
@@ -12,6 +12,7 @@ val Pink40 = Color(0xFF7D5260)
 
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
+val Gray = Color(0xFF8B8B8B)
 val Red = Color(0xFFFF3D00)
 val Green = Color(0xFFA8DB10)
 val Blue = Color(0xFF1976D2)
