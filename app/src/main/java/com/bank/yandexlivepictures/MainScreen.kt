fd7aea873819374976ce8f67ac9e8e9492a19523
adonixis_cvs <ilya.fomenko@compvisionsys.com>
2024-10-30 18:22:16 +0500

    Add icons and backgtound

---
+++ app/src/main/java/com/bank/yandexlivepictures/MainScreen.kt
@@ -0,0 +1,254 @@
+package com.bank.yandexlivepictures
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bank.yandexlivepictures.ui.theme.Blue
+import com.bank.yandexlivepictures.ui.theme.YandexLivePicturesTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+                .height(32.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                IconButton(
+                    modifier = Modifier
+                        .size(24.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_left_24),
+                        contentDescription = "Undo icon"
+                    )
+                }
+
+                IconButton(
+                    modifier = Modifier
+                        .size(24.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_right_24),
+                        contentDescription = "Redo icon"
+                    )
+                }
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                IconButton(
+                    modifier = Modifier
+                        .size(32.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_bin_32),
+                        contentDescription = "Bin icon"
+                    )
+                }
+
+                IconButton(
+                    modifier = Modifier
+                        .size(32.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_file_plus_32),
+                        contentDescription = "File plus icon"
+                    )
+                }
+
+                IconButton(
+                    modifier = Modifier
+                        .size(32.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_layers_32),
+                        contentDescription = "Layers icon"
+                    )
+                }
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                IconButton(
+                    modifier = Modifier
+                        .size(32.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_stop_32),
+                        contentDescription = "Stop icon"
+                    )
+                }
+
+                IconButton(
+                    modifier = Modifier
+                        .size(32.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_play_32),
+                        contentDescription = "Play icon"
+                    )
+                }
+            }
+        }
+
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(top = 32.dp, bottom = 22.dp)
+                .clip(shape = RoundedCornerShape(20.dp)),
+            painter = painterResource(id = R.drawable.bg_paper),
+            contentDescription = "Paper background",
+            contentScale = ContentScale.Crop
+        )
+
+        Row(
+            modifier = Modifier
+                .padding(bottom = 16.dp) // TODO: Do we need this padding?
+                .height(32.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                modifier = Modifier
+                    .size(32.dp),
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_pencil_32),
+                    contentDescription = "Pencil icon"
+                )
+            }
+
+            IconButton(
+                modifier = Modifier
+                    .size(32.dp),
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_brush_32),
+                    contentDescription = "Brush icon"
+                )
+            }
+
+            IconButton(
+                modifier = Modifier
+                    .size(32.dp),
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_erase_32),
+                    contentDescription = "Erase icon"
+                )
+            }
+
+            IconButton(
+                modifier = Modifier
+                    .size(32.dp),
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_instruments_32),
+                    contentDescription = "Instruments icon"
+                )
+            }
+
+            IconButton(
+                modifier = Modifier
+                    .size(32.dp),
+                onClick = {
+
+                }
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(28.dp)
+                        .background(color = Blue, shape = CircleShape),
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    YandexLivePicturesTheme {
+        MainScreen()
+    }
+}
--- app/src/main/java/com/bank/yandexlivepictures/ui/theme/Theme.kt
+++ app/src/main/java/com/bank/yandexlivepictures/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green,
+    background = Black,
+    onBackground = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Green,
+    background = White,
+    onBackground = Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
